658c1b49411095ff7e8b8528132fecea5b863714
MontblanKodE <arya.yudistira.db@gmail.com>
2022-05-24 08:02:35 +0700

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Home from './Pages';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import SigninPage from './Pages/signin';
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/signin" element={<SigninPage />} exact />
+      </Routes>      
     </Router>
   );
 }
---
+++ src/Pages/signin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SigninPage = () => {
+  return (
+    <h1>VOk</h1>
+  )
+}
+
+export default SigninPage
